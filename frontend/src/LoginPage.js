bb8bd1bdd1a7ad268edc5a0ae570289464e4f394
Fellip15 <fellipalves15@gmail.com>
2022-06-06 12:05:05 -0300

    Coloquei estilos na loginpage

--- frontend/src/LoginPage.js
+++ frontend/src/LoginPage.js
@@ -1,17 +1,24 @@
 import { React, useState } from "react";
+import { BackgroundLogin } from "./style";
+import "./LoginPage.css";
 
 const LoginPage = props => {
     const [user, setUser] = useState("");
     return (
-    <>
+    <BackgroundLogin>
             <h1>LOGIN PAGE</h1>
             <form>
                 <label>Usuario:</label>
-                <input type="text" value={user} onChange={e => setUser(e.target.value)}/>
+                <input type="text" placeholder="username" value={user} onChange={e => setUser(e.target.value)}/>
+                <label>Senha:</label>
+                <input type="text" placeholder="password" onChange={e => setUser(e.target.value)}/>
             </form>
             <br></br>
-        <button onClick={() => props.callback(user)}>LOGIN</button>
-    </>)
+        <div id="buttons-login-cadastrar">
+            <button onClick={() => props.callback(user)}>LOGIN</button>
+            <button onClick={() => {console.log("Quero cadastrar")}}>Cadastrar</button>
+        </div>
+    </BackgroundLogin>)
 }
 
 export default LoginPage
---
+++ frontend/src/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const BackgroundLogin = styled.div`
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    background-color: rgb(0, 126, 199);
+    border-radius: 50px;
+`
