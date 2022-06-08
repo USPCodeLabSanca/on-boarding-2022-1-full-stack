62181b58cef110f10e79da0319b0496786589c76
Gustavo Bhering Grande <gustavobgrande@hotmail.com>
2022-06-07 21:14:52 -0300

    editar titulo da lista de cards feito

--- backend/controllers/CardList.js
+++ backend/controllers/CardList.js
@@ -17,3 +17,13 @@ module.exports.deleteCardList = async (req, res) => {
     const cardListDeleted = await CardListModel.findOneAndDelete({ _id: id }).exec();
     return res.status(200).json(cardListDeleted);
 }
+
+
+module.exports.renameCardList = async (req, res) =>{
+    const id = req.params.id;
+    const new_title = req.body.titulo;
+    const renamedCardList = await CardListModel.findOneAndUpdate({_id : id}, {titulo: new_title}, {new: true}).exec();
+    return res.status(200).json(renamedCardList);
+
+
+}
--- backend/routes/CardList.js
+++ backend/routes/CardList.js
@@ -4,6 +4,7 @@ const CardListController = require('../controllers/CardList')
 
 router.post('/cardList/:id', CardListController.createCardList);
 router.delete('/cardList/:id', CardListController.deleteCardList);
+router.put('/cardList/:id', CardListController.renameCardList);
 //router.get('/cardList', CardListController.getCardsList);
 
 module.exports = router;
