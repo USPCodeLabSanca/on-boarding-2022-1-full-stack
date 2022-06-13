a9e3814de4989c346fea8df44482c0181b03516b
LucasOCastro <lucas_o__castro@hotmail.com>
2022-06-12 22:29:28 -0300

    Conserta comportamento de mudança e exclusão de quadros

--- frontend/src/MainPage.js
+++ frontend/src/MainPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { useCookies } from "react-cookie";
 import axios from 'axios'
 import Condicional from './Components/Condicional'
@@ -8,7 +8,7 @@ import './MainPage.css'
 
 const MainPage = props => {
     const [removeCookie, setCookie] = useCookies(["user"]);
-    const [currState, setState] = useState({"quadros":[], 'pulledQuadros':false, 'pulledListas':false});
+    const [currState, setState] = useState({ "quadros": [], 'pulledQuadros': false, 'pulledListas': false });
 
     function changePulledBoards() {
         let stateCopy = JSON.parse(JSON.stringify(currState));
@@ -16,29 +16,42 @@ const MainPage = props => {
         setState(stateCopy);
     }
 
-    // Olha a variavel quadro no parametro get
-    // Se nenhum quadro ta setado, usa o valor padrao de 0
-    let search = window.location.search;
-    let params = new URLSearchParams(search);
-    let quadroSelecionado;
-    if (!params.get("quadro") || isNaN(params.get("quadro"))) {
-        quadroSelecionado = 0;
-    }
-    else {
-        const parsed = parseInt(params.get("quadro"), 10);
-        quadroSelecionado = (parsed > 0 && parsed < currState.quadros.length) ? parsed : 0;
-    }
-
     if (!currState['pulledQuadros']){
         console.log("pulling quadros")
         axios.get('http://localhost:5300/board')
         .then(function (response) {
-          let quadros = response['data'];
-          let stateCopy = JSON.parse(JSON.stringify(currState));
-          stateCopy.quadros = quadros;
-          stateCopy.pulledQuadros = true;
-          setState(stateCopy)})
+            let quadros = response['data'];
+            let stateCopy = JSON.parse(JSON.stringify(currState));
+            stateCopy.quadros = quadros;
+            stateCopy.pulledQuadros = true;
+            setState(stateCopy)
+        })
     }
+
+    // Olha a variavel quadro no parametro get
+    // Se nenhum quadro ta setado, usa o valor padrao de 0
+    let search = window.location.search;
+    let params = new URLSearchParams(search);
+    let paramQuadro = params.get("quadro");
+    let quadroSelecionado;
+    if (!paramQuadro || isNaN(paramQuadro)) {
+        quadroSelecionado = 0;
+    }
+    else {
+        let parsed = parseInt(paramQuadro, 10);
+        if (parsed < 0 || parsed >= currState.quadros.length) {
+            parsed = (parsed < 0) ? 0 : currState.quadros.length - 1;
+            if (currState.quadros.length > 0) {
+                params.set("quadro", parsed);
+                window.location.search = params;    
+            }
+        }
+        quadroSelecionado = parsed;
+    }
+    
+    
+
+    
 
     function handleRemoveCookie() {
         setCookie("user", '', {path:'/'});
